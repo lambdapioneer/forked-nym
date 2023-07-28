eca27e0f2bf0ce3397fc556ef7551232b9f1b33b
Daniel Hugenroth <daniel.hugenroth@gmail.com>
2023-07-28 12:05:11 +0100

    Allow to create mix packets using SURBs and then reflect them

--- sdk/rust/nym-sdk/examples/packet_factory_with_surbs.rs
+++ sdk/rust/nym-sdk/examples/packet_factory_with_surbs.rs
@@ -15,22 +15,27 @@ async fn main() {
     let bob_address = bob.nym_address();
     println!("bob_address={bob_address}");
 
-    // Alice creates packets for bob
+    // that's Charlie: they create the SURBS and later the packets
+    let charlie = mixnet::MixnetClient::connect_new().await.unwrap();
+    let charlie_address = charlie.nym_address();
+    println!("charlie_address={charlie_address}");
+
+    // Charlie (e.g. the discover node) creates some SURBs
+    let surbs = charlie.create_surbs(&bob_address, b"nonce".to_vec(), 5).await.unwrap();
+
+    // Alice then takes those SURBs and creates packets for Bob
     // (using a long message that requires fragmentation)
-    let packets = alice.create_mix_packet(
+    let packets = alice.create_mix_packet_with_surbs(
         [42u8; 4000],
-        &bob_address,
+        surbs,
     ).await.unwrap();
-    println!("{:?}", packets);
+    println!("packets={:?}", packets);
+
+    // Charlie then sends those packets to Bob (i.e. being a reflecting node)
 
     // serialize and deserialize
     let serialized_packets: Vec<Vec<u8>> = packets.into_iter().map(|x| x.into_bytes().unwrap()).collect();
     let packets = serialized_packets.into_iter().map(|x| MixPacket::try_from_bytes(x.as_slice()).unwrap()).collect();
-
-    // that's Charlie: they send the packets
-    let charlie = mixnet::MixnetClient::connect_new().await.unwrap();
-    let charlie_address = charlie.nym_address();
-    println!("charlie_address={charlie_address}");
 
     // let's do the actual sending
     charlie.send_packets(packets).await;
